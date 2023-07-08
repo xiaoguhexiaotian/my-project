27d181584d08cb7c62aee054502240a0f854cf45
chenjianhong <574720759@qq.com>
2023-07-08 18:24:04 +0800

    feat：左侧布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,9 @@
-<script lang="ts" setup>
-let list = [
-  {
-    name: 'test1',
-    types: [
-      { name: 'Type1', type: 1 },
-      { name: 'Type2', type: 2 },
-      { name: 'Type3', type: 3 }
-    ]
-  },
-  {
-    name: 'test2',
-    types: [
-      { name: 'TypeA', type: 'A' },
-      { name: 'TypeB', type: 'B' }
-    ]
-  },
-  {
-    name: 'test3',
-    types: [
-      { name: 'TypeX', type: 'C' },
-      { name: 'TypeY', type: 'D' }
-    ]
-  }
-]
-
-let activeKey = convertListToObject(list)
-
-function convertListToObject(list) {
-  const result = {}
-  for (const item of list) {
-    const { name, types } = item
-    const defaultValue = types.length > 0 ? types[0].type : undefined
-    result[name] = defaultValue
-  }
-  return result
-}
-const test = (v, v2) => {
-  activeKey[v2] = v
-  console.log(v, activeKey)
-}
+<script setup lang="ts">
+import Layout from '../layout/default/Layout.vue'
 </script>
 
 <template>
-  <div v-for="i in list">
-    <div :id="activeKey[i.name]">
-      <span v-for="k in i.types" style="margin-right: 30px" @click="test(k.type, i.name)">{{
-        k.name
-      }}</span>
-    </div>
-  </div>
+  <Layout />
 </template>
 
-<style lang="less" scoped></style>
+<style scoped lang="less"></style>
