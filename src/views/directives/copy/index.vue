ed987039289ce1859e009bed6f1559026e18ce38
chenjianhong <574720759@qq.com>
2023-08-06 02:31:26 +0800

    feat:优化copy指令

--- src/views/directives/copy/index.vue
+++ src/views/directives/copy/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <span class="copy"> {{ data }}</span>
-    <Button v-copy>copy</Button>
+    <Button v-copy="0">copy</Button>
   </div>
   <Textarea class="copy" v-model:value="data2" style="width: 180px" />
-  <Button v-copy class="copy2">copy2</Button>
-  <div class="copy copy3" v-copy>这是一个单独的div盒子</div>
+  <Button v-copy="1">copy2</Button>
+  <div class="copy" v-copy="2">这是一个单独的div盒子</div>
 
-  <h1 class="text-24px copy copy4" v-copy>copy自定义指令</h1>
+  <h1 class="text-24px copy" v-copy="3">copy自定义指令</h1>
   <Codemirror :code="code" width="95%" height="600px" :isShowBtn="false" />
 </template>
 <script setup lang="ts">
@@ -21,17 +21,21 @@ const code = ref(`/**
  * v-copy
  * 复制某个值至剪贴板
  */
-import type { Directive } from 'vue'
+import type { Directive, DirectiveBinding } from 'vue'
 import { message } from 'ant-design-vue'
 
 interface ElType extends Element {
   copyData?: string | number
+  copyIndex?: number | string
   __handleClick__?: any
   value?: string
 }
 
 const copy: Directive = {
-  mounted(el: ElType) {
+  mounted(el: ElType, binding: DirectiveBinding) {
+    el.copyIndex = binding.value
+    // 传值必须为number了类型或者字符串类型的数字
+    if (Number.isNaN(Number(binding.value))) throw new Error('v-copy指令值类型错误')
     el.addEventListener('click', handleCopy)
   },
   beforeUnmount(el: ElType) {
@@ -43,24 +47,18 @@ async function handleCopy(this: any) {
   // 获取页面上所有有copy类名的元素
   const allTarget = document.querySelectorAll('.copy')
   // 具体复制的元素的下标
-  let index: number = 0
-  // copy目标
-  let copyTarget: ElType
-  // 当页面上多个元素都需要复制时，需要给绑定指令的元素添加类名，且类名必须在最后。规则为复制的内容在所有用有copy类名的顺序，例如页面上第二个有copy的元素，则添加copy2类名
-  // 判断指令元素是否添加了类名，如有添加则需要找到对应的下标，这样才能正确的找到需要copy的目标
-  if (this.className.indexOf('copy') > -1) {
-    let classList = this.className.split(' ').pop()
-    index = Number(classList.slice(-1) - 1)
-    // 当index大于copy类名的数量时或者为NaN时说明使用错误
-    if (index >= allTarget.length || isNaN(index)) {
-      throw new Error(\`\${this.className}指令类名错误\`)
-    }
+  let index: number = this.copyIndex
+  // 当index大于copy类名的数量时或者为NaN时说明使用错误
+  if (index >= allTarget.length || isNaN(index)) {
+    throw new Error('v-copy指令传值错误')
   }
-  index == 0 ? (copyTarget = allTarget![0]) : (copyTarget = allTarget![index])
+  // copy目标
+  const copyTarget: ElType = allTarget![index]
   // copy数据
   let copyData: string
   // DOM结构上存在value说明有绑定的值，这时候直接取value，否则直接取DOM结构的textContent
   copyTarget!.value ? (copyData = copyTarget!.value) : (copyData = copyTarget!.textContent!)
+
   // 复制到剪贴板
   await navigator.clipboard
     .writeText(copyData)
@@ -71,6 +69,7 @@ async function handleCopy(this: any) {
 }
 
 export default copy
+
 `)
 </script>
 
