140952921898aae2e972c549864a59030e14a86c
chenjianhong <574720759@qq.com>
2023-12-15 17:03:12 +0800

    feat: 添加pre-commit校验js,jsx,ts,tsx,vue,json文件，自动根据prettier修复格式化后提交

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,22 +11,22 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'perf',
-        'style',
-        'docs',
-        'test',
-        'refactor',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-        'wip',
-        'workflow',
-        'types',
-        'release',
-      ],
-    ],
-  },
-};
+        'feat', // 新功能
+        'fix', // 修补bug
+        'perf', // 优化相关，比如提升性能、体验
+        'style', // 代码格式修改, 注意不是 css 修改
+        'docs', // 文档修改
+        'test', // 测试用例修改
+        'refactor', // 重构
+        'build', // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
+        'ci', // 持续集成修改
+        'chore', // 其他改动
+        'revert', // 代码回滚
+        'wip', // 表示正在进行中的工作，通常用于标记尚未完成的任务或功能。
+        'workflow', // 表示工作流程或流程，指项目中的某个特定步骤或顺序。
+        'types', // 表示类型或数据结构，指项目中定义的一些自定义类型或类。
+        'release' // 表示发布或版本控制，指将项目部署到生产环境或发布新版本的功能。
+      ]
+    ]
+  }
+}
--- src/views/noPage/index.vue
+++ src/views/noPage/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="no-page">无此页面</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref} from 'vue'
+console.log(1,           2,          3,          4)
+</script>
 
 <style scoped lang="less"></style>
